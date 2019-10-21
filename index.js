cafd718611a863e8f59890bf030f077e12c93f25
EdwinBoersema <edwinboersema@gmail.com>
2019-10-21 12:57:14 +0200

    Added a campgrounds.ejs file and link.

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ app.get("/", (req, res) => {
 });
 
 //Campgrounds route
-app.get("campgrounds", (req, res) => {
+app.get("/campgrounds", (req, res) => {
     const campgrounds = [
         {name: "Salmon Creek", image: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/49549058324311.59f8339842af3.png"},
         {name: "Thornbury Hills", image: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/0e7f3f60632151.5a544ee6a44c7.jpg"},
         {name: "Sunset Plains", image: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/54999c60632151.5a5450ae5dd49.jpg"}
     ];
-    res.render("campgrounds");
+    res.render("campgrounds", {campgrounds: campgrounds});
 });
 
 // starting the server on port 3000
