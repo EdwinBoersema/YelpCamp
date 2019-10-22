5398fcf8d5fb5c5b32d83d59f9de22cf38182ff3
EdwinBoersema <edwinboersema@gmail.com>
2019-10-22 09:58:39 +0200

    Added mongodb and removed the hardcoded campgrounds array.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/yelp_camp");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,16 +14,42 @@ app.get("/", (req, res) => {
     res.render("landing");
 });
 
+// Schema setup
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+const Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({ 
+//     name: "Thornbury Hills", 
+//     image: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/0e7f3f60632151.5a544ee6a44c7.jpg" 
+// }, (err, campground) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("New campground created:");
+//         console.log(campground);
+//     }
+// });
+
 // campgrounds array
-const campgrounds = [
-    { name: "Salmon Creek", image: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/49549058324311.59f8339842af3.png" },
-    { name: "Thornbury Hills", image: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/0e7f3f60632151.5a544ee6a44c7.jpg" },
-    { name: "Sunset Plains", image: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/54999c60632151.5a5450ae5dd49.jpg" }
-];
+// const campgrounds = [
+//     { name: "Salmon Creek", image: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/49549058324311.59f8339842af3.png" },
+//     { name: "Thornbury Hills", image: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/0e7f3f60632151.5a544ee6a44c7.jpg" },
+//     { name: "Sunset Plains", image: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/54999c60632151.5a5450ae5dd49.jpg" }
+// ];
 
 //Campgrounds route
 app.get("/campgrounds", (req, res) => {
-    res.render("campgrounds", { campgrounds: campgrounds });
+    Campground.find({}, (err, allCampgrounds) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds", {campgrounds: allCampgrounds});
+        }
+    });
 });
 
 app.post("/campgrounds", (req, res) => {
